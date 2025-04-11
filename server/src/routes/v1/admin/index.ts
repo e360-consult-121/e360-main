6564a3f24994ba4e6ac58860d6cf5fa6b5c50747
pankaj2312002 <pankaj707165@gmail.com>
2025-04-11 12:27:25 +0530

    Route mounting error solved

--- server/src/routes/v1/admin/index.ts
+++ server/src/routes/v1/admin/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import visaTypeRoutes from "./visaTypeRoutes";
-
+import leadRoutes from "./leadRoutes";
+import consultationRoutes from "./consultationRoutes";
+import paymentRoutes from "./paymentRoutes";
 const router = Router();
 
 router.use("/visaType", visaTypeRoutes);
-
+router.use("/leads" , leadRoutes);
+router.use("/consultations" , consultationRoutes);
+router.use("/payment" , paymentRoutes);
 
 export default router;
