6c32b5ccc560f2e17edfb3de20d43ac06b370acb
pankaj2312002 <pankaj707165@gmail.com>
2025-04-11 23:07:54 +0530

    stripe part commented

--- server/src/controllers/Leads/paymentController.ts
+++ server/src/controllers/Leads/paymentController.ts
@@ -1,80 +1,80 @@
-import { NextFunction, Request, Response } from "express";
-import Stripe from 'stripe';
-import AppError from "../../utils/appError";
-import { LeadModel } from "../../leadModels/leadModel";
-import {PaymentModel} from "../../leadModels/paymentModel"
-import { leadStatus } from "../../types/enums/enums";
-import { paymentStatus } from "../../types/enums/enums"
-import { sendEmail } from "../../utils/sendEmail";
+// import { NextFunction, Request, Response } from "express";
+// import Stripe from 'stripe';
+// import AppError from "../../utils/appError";
+// import { LeadModel } from "../../leadModels/leadModel";
+// import {PaymentModel} from "../../leadModels/paymentModel"
+// import { leadStatus } from "../../types/enums/enums";
+// import { paymentStatus } from "../../types/enums/enums"
+// import { sendEmail } from "../../utils/sendEmail";
 
 
-// instance of Stripe
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2025-03-31.basil', // new Basil SDK version (version compatability issue aa sakte hai)
-});
+// // instance of Stripe
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+//   apiVersion: '2025-03-31.basil', // new Basil SDK version (version compatability issue aa sakte hai)
+// });
 
-// send payment link
-export const sendPaymentLink = async (req: Request, res: Response) => {
+// // send payment link
+// export const sendPaymentLink = async (req: Request, res: Response) => {
 
-    const  leadId  = req.params.leadId;
-    const {  amount } = req.body;
+//     const  leadId  = req.params.leadId;
+//     const {  amount } = req.body;
   
-    const lead = await LeadModel.findById(leadId);
-    if (!lead) {
-      res.status(404);
-      throw new Error("Lead not found");
-    }
+//     const lead = await LeadModel.findById(leadId);
+//     if (!lead) {
+//       res.status(404);
+//       throw new Error("Lead not found");
+//     }
 
-    // logic for creating paymentLink
-    // 1. Create a product (optional — can be reused)
-    const product = await stripe.products.create({
-      name: `Visa Consultation for ${lead.fullName.first} ${lead.fullName.last}`,
-    });
+//     // logic for creating paymentLink
+//     // 1. Create a product (optional — can be reused)
+//     const product = await stripe.products.create({
+//       name: `Visa Consultation for ${lead.fullName.first} ${lead.fullName.last}`,
+//     });
     
-    // 2. Create a price for that product
-    const price = await stripe.prices.create({
-      unit_amount: amount * 100, // amount in paisa
-      currency: "inr",
-      product: product.id,
-    });
+//     // 2. Create a price for that product
+//     const price = await stripe.prices.create({
+//       unit_amount: amount * 100, // amount in paisa
+//       currency: "inr",
+//       product: product.id,
+//     });
     
-    // 3. Create the payment link using the price ID
-    const paymentLink = await stripe.paymentLinks.create({
-      line_items: [
-        {
-          price: price.id, //  this is now a string, as required
-          quantity: 1,
-        },
-      ],
-      metadata: {
-        leadId: leadId,
-        email: lead.email,
-      },
-    });
+//     // 3. Create the payment link using the price ID
+//     const paymentLink = await stripe.paymentLinks.create({
+//       line_items: [
+//         {
+//           price: price.id, //  this is now a string, as required
+//           quantity: 1,
+//         },
+//       ],
+//       metadata: {
+//         leadId: leadId,
+//         email: lead.email,
+//       },
+//     });
 
-    const html = `
-    <p>Hi ${lead.fullName.first},</p>
-    <p>Please complete the payment to start your visa application:</p>
-    <a href="${paymentLink.url}" target="_blank">${paymentLink.url}</a>
-    <p>If you've already paid, please ignore this.</p>
-    `;
+//     const html = `
+//     <p>Hi ${lead.fullName.first},</p>
+//     <p>Please complete the payment to start your visa application:</p>
+//     <a href="${paymentLink.url}" target="_blank">${paymentLink.url}</a>
+//     <p>If you've already paid, please ignore this.</p>
+//     `;
   
-    await sendEmail({
-      to: lead.email,
-      subject: "Complete Your Payment to start your VisaApplication",
-      html,
-    });
+//     await sendEmail({
+//       to: lead.email,
+//       subject: "Complete Your Payment to start your VisaApplication",
+//       html,
+//     });
   
-    res.status(200).json({ success: true, url: paymentLink.url });
-};
+//     res.status(200).json({ success: true, url: paymentLink.url });
+// };
 
 
 
 
-// calendly webhook
-export const stripeWebhook = async (req: Request, res: Response) => {
-// logic here ...
-};
+// // calendly webhook
+// export const stripeWebhook = async (req: Request, res: Response) => {
+// // logic here ...
+// };
 
 
 
--- server/src/routes/v1/admin/paymentRoutes.ts
+++ server/src/routes/v1/admin/paymentRoutes.ts
@@ -1,12 +1,12 @@
-import { Router } from "express";
-import { authenticate ,authorizeAdmin } from "../../../middlewares/authenticate";
-import asyncHandler from "../../../utils/asyncHandler";
-import * as paymentControllers from "../../../controllers/Leads/paymentController";
+// import { Router } from "express";
+// import { authenticate ,authorizeAdmin } from "../../../middlewares/authenticate";
+// import asyncHandler from "../../../utils/asyncHandler";
+// import * as paymentControllers from "../../../controllers/Leads/paymentController";
 
 
-const router = Router();
+// const router = Router();
 
-router.post("/:leadId/sendPaymentLink", authenticate , authorizeAdmin , asyncHandler(paymentControllers.sendPaymentLink));
-router.post("/webhook/stripe", asyncHandler(paymentControllers.stripeWebhook));
+// router.post("/:leadId/sendPaymentLink", authenticate , authorizeAdmin , asyncHandler(paymentControllers.sendPaymentLink));
+// router.post("/webhook/stripe", asyncHandler(paymentControllers.stripeWebhook));
 
-export default router;
+// export default router;
