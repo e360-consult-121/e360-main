fa3bb876501d5e38a95bbdc3212d986ac8c032d9
pankaj2312002 <pankaj707165@gmail.com>
2025-04-11 14:11:10 +0530

    New Jotform File

--- server/src/routes/v1/admin/index.ts
+++ server/src/routes/v1/admin/index.ts
@@ -3,11 +3,13 @@ import visaTypeRoutes from "./visaTypeRoutes";
 import leadRoutes from "./leadRoutes";
 import consultationRoutes from "./consultationRoutes";
 import paymentRoutes from "./paymentRoutes";
+// import jotformRoutes from "./jotformRoutes"
 const router = Router();
 
 router.use("/visaType", visaTypeRoutes);
 router.use("/leads" , leadRoutes);
 router.use("/consultations" , consultationRoutes);
 router.use("/payment" , paymentRoutes);
+// router.use("/jotforms" , jotformRoutes);
 
 export default router;
