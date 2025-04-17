542319ee0abeb514e405a0ac80150faefec97a21
pankaj2312002 <pankaj707165@gmail.com>
2025-04-17 18:27:28 +0530

    markconsultation error resolved

--- server/src/routes/v1/admin/consultationRoutes.ts
+++ server/src/routes/v1/admin/consultationRoutes.ts
@@ -8,6 +8,6 @@ const router = Router();
 
 router.get("/fetchAllConsultations", authenticate , authorizeAdmin , asyncHandler(consultationControllers.getAllConsultations));
 router.post("/:leadId/sendConsultationLink", authenticate , authorizeAdmin , asyncHandler(consultationControllers.sendConsultationLink));
-router.post("/:consultationId/markConsultationAsCompleted", authenticate , authorizeAdmin , asyncHandler(consultationControllers.sendConsultationLink));
+router.post("/:consultationId/markConsultationAsCompleted", authenticate , authorizeAdmin , asyncHandler(consultationControllers.markConsultationAsCompleted));
 router.post("/webhook/calendly", asyncHandler(consultationControllers.calendlyWebhook));
 export default router;
