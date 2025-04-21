2d1bab919b98cab05be495fb7d1231293c08a45d
Manish <[manishbulchandani4@gmail.com]>
2025-04-21 12:32:48 +0530

    priority functions layout

---
+++ server/src/priorityFunctions/domiGrena.ts
@@ -0,0 +1,7 @@
+import { ParsedxxxData } from "../parsingFunctions/domiGrenaParse";
+import { leadPriority } from "../types/enums/enums";
+
+export const getDomiGrenaPriority=(data:ParsedxxxData):leadPriority=>{
+
+    return leadPriority.HIGH
+}
---
+++ server/src/priorityFunctions/dubaiPriority.ts
@@ -0,0 +1,7 @@
+import { ParsedDubaiData } from "../parsingFunctions/dubaiParse";
+import { leadPriority } from "../types/enums/enums";
+
+export const getDubaiPriority=(data:ParsedDubaiData):leadPriority=>{
+    
+    return leadPriority.HIGH
+}
