60d8974fafda8c7015643ee15084ae0a23e10a47
Manish <[manishbulchandani4@gmail.com]>
2025-06-08 19:20:02 +0530

    build issue fix

--- server/src/routes/v1/admin/bankdetailsRoutes.ts
+++ server/src/routes/v1/admin/bankdetailsRoutes.ts
@@ -1,17 +1,28 @@
 import { Router } from "express";
-import { authenticate ,authorizeAdmin } from "../../../middlewares/authenticate";
+import {
+  authenticate,
+  authorizeAdmin,
+} from "../../../middlewares/authenticate";
 import { checkPermission } from "../../../middlewares/permissionMiddleware";
 import asyncHandler from "../../../utils/asyncHandler";
-import * as bankDetails from "../../../controllers/manageBankDetails/bankDetailsControllers"
+import * as bankDetails from "../../../controllers/manageBankDetails/bankDetailsControllers";
 
 const router = Router();
 
-router.get("/fetchBankDetails",
-    authenticate ,authorizeAdmin,
-    // checkPermission("View Bank Details"),
-    asyncHandler(bankDetails.getBankDetails));
+router.get(
+  "/fetchBankDetails",
+  authenticate,
+  authorizeAdmin,
+  // checkPermission("View Bank Details"),
+  asyncHandler(bankDetails.getBankDetails)
+);
 
-router.put("/editBankDetails/:visaTypeName", 
-authenticate ,authorizeAdmin,
-    // checkPermission("Edit Bank Details"),
- asyncHandler(bankDetails.editBankDetails))
+router.put(
+  "/editBankDetails/:visaTypeName",
+  authenticate,
+  authorizeAdmin,
+  // checkPermission("Edit Bank Details"),
+  asyncHandler(bankDetails.editBankDetails)
+);
+
+export default router;
