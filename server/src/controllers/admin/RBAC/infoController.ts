fc42ee2fe77bcfd58345b05bfe5daaac52a463b1
pankaj2312002 <pankaj707165@gmail.com>
2025-06-18 15:44:00 +0530

    edit and delete role api added

--- server/src/controllers/admin/RBAC/infoController.ts
+++ server/src/controllers/admin/RBAC/infoController.ts
@@ -53,20 +53,7 @@ export const fetchAllFeatures = async (req: Request, res: Response) => {
     });
   };
 
-
-
   
-
-
-// export const fetchAllRoles = async (req: Request, res: Response) => {
-//     const roles = await roleModel.find().sort({ roleName: 1 }); // sorted alphabetically
-  
-//     res.status(200).json({
-//       success: true,
-//       count: roles.length,
-//       roles,
-//     });
-// };
 
 export const fetchAllRoles = async (req: Request, res: Response) => {
   const roles = await roleModel.find().sort({ roleName: 1 });
